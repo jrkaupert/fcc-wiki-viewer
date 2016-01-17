ecc13d93f06e2f239cec27f947f93dcbb2f5c929
James R. Kaupert <jrkaupert@gmail.com>
2016-01-17 09:27:01 -0500

    final wiki viewer updates

--- scripts/wikiviewer.js
+++ scripts/wikiviewer.js
@@ -1,36 +1,53 @@
-// TODO make search button work!
-// TODO make search suggestions work for search bar
-// TODO add random search button
-
-function queryWiki() {
-  var site = "https://en.wikipedia.org";
-  // var queryString = "/w/api.php?action=query&list=search&format=json&srsearch=apple&srprop=snippet%7Csectionsnippet&generator=search&redirects=&gsrsearch=apple&gsrprop=snippet";
-  var searchTerm = "apple";
-
-  var queryString2 = "/w/api.php?action=opensearch&format=json&search="+searchTerm+"&limit=10&suggest=&redirects=resolve&format=json";
-
-  var queryEnd = "&formatversion=2&callback=?";
-
-
-  $.getJSON(site + queryString2 + queryEnd, function(response) {
-
-    var divId;
-    var titleStr;
-    var snippetStr;
-    for (var i = 0; i < 10; i++) {
-
-      titleStr = response[1][i];
-      snippetStr = response[2][i];
-      link = response[3][i];
-
-      divId = "#r" + (i + 1).toString();
-      $(divId).append("<h4>" + titleStr + "</h4>" + "<p>" + snippetStr + "</p>")
-      $(divId).parent('a').attr("href", link)
-    }
-  });
-}
 
 $(document).ready(function() {
-  queryWiki();
 
+  $(window).on('keydown',function(ev) {
+    if(ev.keyCode == 13) {
+      ev.preventDefault();
+      return false;
+    }
+  });
+
+  var changeTimer = false;
+  $('input').on('keyup',function() {
+
+    var searchTerm = document.getElementById("searchText").value;
+
+    var site = "https://en.wikipedia.org";
+    var queryString = "/w/api.php?action=opensearch&format=json&search="+searchTerm+"&limit=10&suggest=&redirects=resolve&format=json";
+
+    var queryEnd = "&formatversion=2&callback=?";
+
+    if (changeTimer !== false) {
+      clearTimeout(changeTimer);
+    }
+      changeTimer = setTimeout(function() {
+
+        $.getJSON(site + queryString + queryEnd, function(response) {
+
+          var divId, titleStr, snippetStr;
+          var max = Math.min(10,response[1].length);
+
+          if (searchTerm === "") {
+            for (var ind = 0;ind<10;ind++) {
+              $("#r" + (ind + 1).toString()).hide();
+            }
+          }
+
+          for (var i = 0; i < max; i++) {
+
+            titleStr = response[1][i];
+            snippetStr = response[2][i];
+            link = response[3][i];
+
+            divId = $("#r" + (i + 1).toString());
+            divId.html('').show();
+            divId.append("<h4 class='wikiTitle'>" + titleStr + "</h4>" + "<p class='wikiSnippet'>" + snippetStr + "</p>")
+            divId.parent('a').attr("href", link)
+          }
+        });
+
+        changeTimer = false;
+      },300);
+  });
 });
